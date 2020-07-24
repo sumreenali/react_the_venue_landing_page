7a05983a1c11d635afe69dc70949f30a33c02367
sumreenali <sumreenali11@gmail.com>
2020-07-24 21:53:59 +0500

    on scroll

--- src/components/header_footer/SideDrawer.js
+++ src/components/header_footer/SideDrawer.js
@@ -1,33 +1,43 @@
 import React from "react";
+import { scroller } from "react-scroll";
+
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 
 const SideDrawer = ({ open, onClose }) => {
+  const scrollToElement = (element) => {
+    scroller.scrollTo(element, {
+      duration: 1500,
+      delay: 100,
+      smooth: true,
+    });
+  };
+
   return (
     <Drawer anchor="right" open={open} onClose={() => onClose(false)}>
       <List>
-        <ListItem button onClick={() => console.log("Featured")}>
+        <ListItem button onClick={() => scrollToElement("featured")}>
           {" "}
           Events starts in
         </ListItem>
 
-        <ListItem button onClick={() => console.log("Venue NFO")}>
+        <ListItem button onClick={() => scrollToElement("venueNFO")}>
           {" "}
           Venue NFO
         </ListItem>
 
-        <ListItem button onClick={() => console.log("Highlights")}>
+        <ListItem button onClick={() => scrollToElement("highlights")}>
           {" "}
           Highlights
         </ListItem>
 
-        <ListItem button onClick={() => console.log("Pricing")}>
+        <ListItem button onClick={() => scrollToElement("pricing")}>
           {" "}
           Pricing
         </ListItem>
 
-        <ListItem button onClick={() => console.log("Location")}>
+        <ListItem button onClick={() => scrollToElement("location")}>
           {" "}
           Location
         </ListItem>
